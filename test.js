90f55d32ec3b0f506aaabfe5a68ced5e3c2591b1
Nicholas Gagne <nick@ibloomstudios.com>
2026-03-17 18:24:23 -0400

    Upgrade mocha, fix vulns

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 const promiseAllForgiving = require('./index');
 
-describe('promise-all-forgiving', done => {
+describe('promise-all-forgiving', () => {
   it('should return a promise which resolves when all promises have resolved', () => {
     const promise1 = Promise.resolve('one');
     const promise2 = Promise.resolve('two');
 
-    promiseAllForgiving([promise1, promise2]).then(data => {
+    return promiseAllForgiving([promise1, promise2]).then(data => {
       assert.strictEqual(data.length, 2);
       assert.strictEqual(data[0], 'one');
       assert.strictEqual(data[1], 'two');
@@ -17,7 +17,7 @@ describe('promise-all-forgiving', done => {
     const promise1 = Promise.resolve('one');
     const promise2 = Promise.reject('two');
 
-    promiseAllForgiving([promise1, promise2]).then(data => {
+    return promiseAllForgiving([promise1, promise2]).then(data => {
       assert.strictEqual(data.length, 2);
       assert.strictEqual(data[0], 'one');
       assert.strictEqual(data[1], 'two');
@@ -25,7 +25,7 @@ describe('promise-all-forgiving', done => {
   });
 
   it('should return a promise which resolves with no promises', () => {
-    promiseAllForgiving([]).then(data => {
+    return promiseAllForgiving([]).then(data => {
       assert.strictEqual(data.length, 0);
     });
   });
